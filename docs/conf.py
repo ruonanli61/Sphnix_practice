d3451806e2eeaae0e7064bcd697734785fd47bff
ruonanli61 <163012638+ruonanli61@users.noreply.github.com>
2024-04-15 10:25:51 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,10 +47,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes. 
 # This has been changed to the third part theme sphinx_rtd_theme. Need to install it first!
-# import sphinx-rtd-theme
 
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'pyramid'
+import sphinx-rtd-theme
+
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
+
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'pyramid'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
